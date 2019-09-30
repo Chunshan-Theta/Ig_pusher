1b97b59893708a8ed116bc091edf22e900e6ba5e
Theta Wang <thetawang@ThetadeMac-mini.local>
2019-09-30 23:05:35 +0800

    update

--- examples/login-default.py
+++
@@ -1,9 +0,0 @@
-from instapy_cli import client
-
-username = 'just.test.pusher'
-password = '00000000'
-
-with client(username, password) as cli:
-    # do stuffs with cli
-    ig = cli.api()
-    print(ig.current_user())
